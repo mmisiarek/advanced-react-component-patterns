ca527a081efeeb09b9c02db61ba57829bf466084
michal-misiarek <michal.misiarek@schibsted.pl>
2018-03-01 16:02:57 +0100

    simple render props approach

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
 import React from "react";
 import "./App.css";
 
-import Toggle, { withToggle } from "./Toggle";
-
-const MyToggle = withToggle(({ toggle: { on, toggle } }) => (
-  <button onClick={toggle}>{on ? "on" : "off"}</button>
-));
-
-const MyEventComponent = withToggle(({ toggle, on, event }) => {
-  const props = { [event]: on };
-  return toggle.on ? <button {...props}>The {event} event</button> : null;
-});
+import Toggle from "./Toggle";
+import Switch from "./Switch";
+function MyToggle({ on, toggle }) {
+  return <button onClick={toggle}>{on ? "on" : "off"}</button>;
+}
 
 function App() {
   return (
-    <Toggle onToggle={on => console.log("toggle", on)}>
-      <div>
-        <MyToggle />
-        <hr />
-        <Toggle.On>Button is on</Toggle.On>
-        <Toggle.Off>Button is off</Toggle.Off>
-        <Toggle.Button />
-        <MyEventComponent event="onClick" on={e => alert(e.type)} />
-      </div>
-    </Toggle>
+    <Toggle
+      onToggle={on => console.log("toggle", on)}
+      render={({ on, toggle }) => (
+        <div>
+          {on ? "The button is on" : "The button is off"}
+          <Switch on={on} onClick={toggle} />
+          <hr />
+          <MyToggle on={on} toggle={toggle} />
+        </div>
+      )}
+    />
   );
 }
 
--- src/Toggle.js
+++ src/Toggle.js
@@ -1,68 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Switch from "./Switch";
-
-const TOGGLE_CONTEXT = "__toggle__";
-
-export const withToggle = Component => {
-  const Wrapper = (props, context) => {
-    const toggleContext = context[TOGGLE_CONTEXT];
-    return <Component toggle={toggleContext} {...props} />;
-  };
-
-  Wrapper.contextTypes = {
-    [TOGGLE_CONTEXT]: PropTypes.object.isRequired
-  };
-
-  return Wrapper;
-};
-
-const ToggleOn = withToggle(({ children, toggle: { on } }) => {
-  return on ? children : null;
-});
-
-const ToggleOff = withToggle(({ children, toggle: { on } }) => {
-  return !on ? children : null;
-});
-
-const ToggleButton = withToggle(
-  ({ toggle: { on, toggle }, ...props }, context) => {
-    return <Switch on={on} onClick={toggle} {...props} />;
-  }
-);
 
 class Toggle extends React.Component {
-  static On = ToggleOn;
-  static Off = ToggleOff;
-  static Button = ToggleButton;
-
   static defaultProps = { onToggle: () => {} };
-
-  static childContextTypes = {
-    [TOGGLE_CONTEXT]: PropTypes.object.isRequired
-  };
-
   state = { on: false };
-  toggle = () => {
+  toggle = () =>
     this.setState(
       ({ on }) => ({ on: !on }),
-      () => {
-        this.props.onToggle(this.state.on);
-      }
+      () => this.props.onToggle(this.state.on)
     );
-  };
-
-  getChildContext() {
-    return {
-      [TOGGLE_CONTEXT]: {
-        on: this.state.on,
-        toggle: this.toggle
-      }
-    };
-  }
-
   render() {
-    return <div>{this.props.children}</div>;
+    return this.props.render({
+      on: this.state.on,
+      toggle: this.toggle
+    });
   }
 }
 
